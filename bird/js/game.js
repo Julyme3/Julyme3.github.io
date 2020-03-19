eb8bcd4ee00520d2d0388a672755d27aea401a97
Julyme3 <jg0311@bk.ru>
2020-03-19 21:38:22 +0300

    changed href

--- bird/js/game.js
+++ bird/js/game.js
@@ -55,8 +55,7 @@
             }
     
             if (posX + bird.width >= pipe[i].x && pipe[i].x + pipeUp.width >= posX && (posY <= pipe[i].y + pipeUp.height || posY + bird.height >= pipe[i].y + pipeUp.height + gap) || posY + bird.height >= bg.height - fg.height) {
-                location.reload();
-                console.log('reload');
+                location.href = location.href;
             }
 
             if (pipe[i].x === 5) {
