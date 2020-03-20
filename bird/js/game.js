a3c2f836950be59dd55e88f020b7a3ba80e190ab
Julyme3 <jg0311@bk.ru>
2020-03-20 21:33:48 +0300

    fix

--- bird/js/game.js
+++ bird/js/game.js
@@ -1,31 +1,31 @@
 (function() {
     var cnv = document.getElementById("canvas");
     var ctx = cnv.getContext ("2d");
-    
+
     var bg = new Image();
     var bird = new Image();
     var fg = new Image();
     var pipeBottom = new Image();
     var pipeUp = new Image();
-    
+
     // Звуковые файлы
     var fly = new Audio();
     var scoreAudio = new Audio();
-    
+
     fly.src = "audio/fly.mp3";
     scoreAudio.src = "audio/score.mp3";
-    
+
     bg.src = "img/bg.png";
     bird.src = "img/bird.png";
     fg.src = "img/fg.png";
     pipeBottom.src = "img/pipeBottom.png";
     pipeUp.src = "img/pipeUp.png";
-    
+
     var gap = 90,
         posY = 200,
         posX = 20,
-        grav = 1;    
-        
+        grav = 1;
+
     var pipe = [];
     pipe[0] = {
         x: cnv.width,
@@ -33,27 +33,28 @@
     };
 
     var score = 0;
-    
+
     function onKeydown() {
         posY -= 25;
         fly.play();
     }
     document.addEventListener('keydown', onKeydown);
-    
+
     function draw() {
+      console.log(posX + '   ' + posY);
         ctx.drawImage(bg, 0, 0);
         for (var i = 0; i < pipe.length; i++) {
             ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
             ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
             pipe[i].x--;
-    
+
             if (pipe[i].x === 125) {
                 pipe.push({
                     x: cnv.width,
                     y: Math.floor((Math.random() * pipeUp.height) - pipeUp.height)
                 });
             }
-    
+
             if (posX + bird.width >= pipe[i].x && pipe[i].x + pipeUp.width >= posX && (posY <= pipe[i].y + pipeUp.height || posY + bird.height >= pipe[i].y + pipeUp.height + gap) || posY + bird.height >= bg.height - fg.height) {
                 //location.reload(false);
                 console.log('reload');
@@ -68,10 +69,10 @@
 
         ctx.font = "24px Tahoma";
         ctx.fillText(`Счет: ${score}`, 20, bg.height - 20);
-    
+
         posY += grav;
         requestAnimationFrame(draw);
     }
-    
-    pipeUp.onload = draw;    
+
+    pipeUp.onload = draw;
 })();
