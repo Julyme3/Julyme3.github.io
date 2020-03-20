ef4095097741710160ba8c8f5f1b0a9c482caa88
Julyme3 <jg0311@bk.ru>
2020-03-20 21:37:29 +0300

    'fix'

--- bird/js/game.js
+++ bird/js/game.js
@@ -56,7 +56,7 @@
             }
 
             if (posX + bird.width >= pipe[i].x && pipe[i].x + pipeUp.width >= posX && (posY <= pipe[i].y + pipeUp.height || posY + bird.height >= pipe[i].y + pipeUp.height + gap) || posY + bird.height >= bg.height - fg.height) {
-                //location.reload(false);
+                location.reload(false);
                 console.log('reload');
             }
 
